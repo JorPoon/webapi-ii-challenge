9cac9dc896311d556e909c101341a78b35765c79
Jor_Poon <jormpoon@gmail.com>
2019-03-19 17:26:20 -0400

    created endpoint to find posts by id

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -1,1 +1,30 @@
 const express = require('express');
+
+const Posts = require('../data/db.js');
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    Posts
+    .find('posts')
+    .then(post => {
+        res.status(200).json(post)
+    })
+    .catch(error => {
+        res.status(500).json({error: 'The posts information cannot be retrieved'})
+    });
+});
+
+router.get('/:id', (req, res) => {
+    const {id} = req.params
+    Posts
+    .findById(id)
+    .then(post => {
+        res.status(200).json(post)
+    })
+    .catch(error => {
+        res.status(500).json({error: 'The specified post is unavaiable'})
+    })
+})
+
+module.exports = router;
